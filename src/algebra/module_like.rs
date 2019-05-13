47a24b5edebf71059fb639fb91a11863726ebe5e
Joshua Smith <anvil775@gmail.com>
2019-05-13 16:39:01 -0500

    documentation

--- src/algebra/module_like.rs
+++ src/algebra/module_like.rs
@@ -2,6 +2,47 @@
 pub use core::ops::{Add, AddAssign, Sub, SubAssign, Neg, Mul, MulAssign, Div, DivAssign, Index, IndexMut};
 use algebra::*;
 
+///
+///A product between two vectors or module elements resulting in a scalar that is semi-linear in both arguments
+///
+///Rigorously, the dot product is a mapping `•:M⨯M->R` from a Ring Module to its base ring such that:
+/// * `(x + y)•z = x•z + y•z`
+/// * `x•(y + z) = x•y + x•y`
+/// * `(c*x)•y = c*(x•y)`
+/// * `x•(c*y) = (x•y)*σ(c)` where `σ:R->R` is some anti-automorphism, usually the complex conjugate
+///   or the identity map
+///
+/// # Notes on definition
+///
+///In pursuit of generality, the definition used here is noticeably less strict than the more commonly
+///dot product definition. Of course, the normal dot product is covered by this definition, but in order to
+///capture a number of other important mathematical operations, some properties have been relaxed
+///for greater abstraction. In particular:
+/// * `x•y` might not equal `y•x`
+/// * `x•(c*y)` does not necessarily equal `c*(x•y)` or even `(x•y)*c` in general
+/// * `x•x` can equal 0
+/// * `x•x` can be negative or non-real
+///
+///However, in exchange, this trait can represent the following in addition to the real inner product:
+/// * The complex and hypercomplex inner products, which have `x•(c*y) = conj(c)*(x•y)`
+/// * The Minkowski space-time interval, which requires the time-basis to square to -1
+/// * The 2D cross product: `[x₁,y₁]•[x₂,y₂] = x₁*y₂ - x₂*y₁` which requires non-symmetry
+/// * Conformal Models of Geometric Algebra, which require vector bases that square to 0
+///
+/// # Additional properties
+///
+///In addition to the base properties, the dot product can have a number of additional restrictions
+///signified here by added the corresponding marker trait:
+/// * A [Reflexive Form](ReflexiveModule) requires that `x•y = 0` implies `y•x = 0`
+/// * If M and R have an involution `'`,
+///     * A [Sesquilinear Form](SesquilinearModule) requires `x•(c*y) = c'*(x•y)`
+///     * A [Hermitian Form](HermitianModule) requires `(x•y)' = y'•x'`
+///     * A [Skew-Hermitian Form](HermitianModule) requires `(x•y)' = -y'•x'`
+/// * A [Bilinear Form](ReflexiveModule) requires that `x•(c*y) = (x•y)*c`
+/// * A [Symmetric Form](ReflexiveModule) requires that `x•y = y•x`
+/// * A [Skew-Symmetric Form](ReflexiveModule) requires that `x•y = -y•x`
+///
+///
 pub trait DotProduct<K: UnitalRing>: RingModule<K> {
     fn dot(self, rhs: Self) -> K;
     #[inline] fn squared(self) -> K {self.clone().dot(self)}
