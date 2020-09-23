508dd51a550be958be39e13bedb8f27e5ca11f05
Joshua Smith <anvil775@gmail.com>
2020-09-23 18:55:16 -0500

    fixed unresolved doc links

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -2,18 +2,18 @@
 //!Traits for sets with binary operations
 //!
 //!Traits in this module have been split into four groups:
-//!* ["Group-Like"](algebra::group_like) mathematical objects with one binary operation and
-//! include structures such as [groups](algebra::MulGroup) and [monoids](algebra::MulMonoid)
-//!* ["Ring-Like"](algebra::ring_like) mathematical objects with two binary operations that
-//! [distribute](algebra::Distributive) over the other. This includes sets like [rings](algebra::Ring),
-//! [semirings](algebra::Semiring), [division rings](algebra::DivisionRing), and [fields](algebra::Field).
+//!* ["Group-Like"](group_like) mathematical objects with one binary operation and
+//! include structures such as [groups](MulGroup) and [monoids](MulMonoid)
+//!* ["Ring-Like"](ring_like) mathematical objects with two binary operations that
+//! [distribute](Distributive) over the other. This includes sets like [rings](Ring),
+//! [semirings](Semiring), [division rings](DivisionRing), and [fields](Field).
 //! In addition, this module contains an added system for integer-like properties like
-//! [divisibility](algebra::Divisibility), [greatest common denominators](algebra::GCD), and
-//! [Euclidean division](algebra::EuclideanDiv).
-//!* ["Module-Like"](algebra::module_like) traits for groups with an added scalar multiplication operation.
-//! This includes [vector spaces](algebra::VectorSpace), [ring modules](algebra::RingModule),
-//! and [algebras](algebra::Algebra) as well as a system for [bilinear forms](algebra::BilinearForm).
-//!* [Integer](algebra::Integer) and [Natural](algebra::Natural) numeric traits
+//! [divisibility](Divisibility), [greatest common denominators](GCD), and
+//! [Euclidean division](EuclideanDiv).
+//!* ["Module-Like"](module_like) traits for groups with an added scalar multiplication operation.
+//! This includes [vector spaces](VectorSpace), [ring modules](RingModule),
+//! and [algebras](Algebra) as well as a system for [bilinear forms](BilinearForm).
+//!* [Integer](Integer) and [Natural](Natural) numeric traits
 //!
 //!For ease of use, members of each module have been re-exported into this one.
 //!
--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -2,11 +2,11 @@
 //!Traits for sets with orderings and metrics
 //!
 //!Traits in this module have been split into three groups:
-//!* ["Ordered"](analysis::ordered) sets and related properties. This includes the
-//! [archimedian property](analysis::ArchimedeanProperty) and ordered algebraic systems.
-//!* Traits for [Real](analysis::Real) and [Complex](analysis::Complex) properties and representations
-//!* ["Metric"](analysis::metric) properties and functions. This includes
-//! [metrics](analysis::Metric), [norms](analysis::Norm), and [inner-products](analysis::InnerProductSpace)
+//!* ["Ordered"](ordered) sets and related properties. This includes the
+//! [archimedian property](ArchimedeanProperty) and ordered algebraic systems.
+//!* Traits for [Real](Real) and [Complex](Complex) properties and representations
+//!* ["Metric"](metric) properties and functions. This includes
+//! [metrics](Metric), [norms](Norm), and [inner-products](InnerProductSpace)
 //!
 //!For ease of use, members of each module have been re-exported into this one.
 //!
